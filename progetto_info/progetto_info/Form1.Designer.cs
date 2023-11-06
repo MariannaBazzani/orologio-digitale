3249ca700642c1e2f4422905b1bf0e17881ce062
MariannaBazzani <150047582+MariannaBazzani@users.noreply.github.com>
2023-11-06 18:13:47 +0100

    Importing project
    Importing all files of the C# project inside the repository.

---
+++ progetto_info/progetto_info/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace progetto_info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.BackColor = System.Drawing.Color.LightGray;
+            this.lblData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblData.Location = new System.Drawing.Point(12, 9);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(35, 13);
+            this.lblData.TabIndex = 0;
+            this.lblData.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblData;
+    }
+}
+
---
+++ progetto_info/progetto_info/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace progetto_info 
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            Paint += new PaintEventHandler(Orologio);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void Orologio(object sender, EventArgs e)
+        {
+            lblData.Location = new Point(ClientSize.Width / 2 - (lblData.Width / 2), ClientSize.Height / 2 - 30);
+
+            Graphics orologio = this.CreateGraphics();
+            int X_centro = ClientSize.Width / 2;
+            int Y_centro = ClientSize.Height / 2;
+            int raggio = Math.Min(ClientSize.Height, ClientSize.Width) / 2 - 40;
+            orologio.FillEllipse(Brushes.Black, X_centro - raggio - 11, Y_centro - raggio - 11, 2 * raggio + 22, 2 * raggio + 22);
+            orologio.FillEllipse(Brushes.DarkGreen, X_centro - raggio - 10, Y_centro - raggio - 10, 2 * raggio + 20, 2 * raggio + 20);
+            orologio.FillEllipse(Brushes.Black, X_centro - raggio - 1, Y_centro - raggio - 1, 2 * raggio + 2, 2 * raggio + 2);
+            orologio.FillEllipse(Brushes.LightGray, X_centro - raggio, Y_centro - raggio, 2 * raggio, 2 * raggio);
+            /* La circonferenza dell'orologio in questo modo si basa sulle 
+             dimensioni del form e togliendo 40 non sfora dalla finestra */
+
+            // Creazione dei puntini
+            for (int i = 0; i < 12; i++)
+            {
+                double punti_angolo = i * Math.PI / 6;
+                double punti_centro_X = X_centro - (raggio * 0.95 * Math.Sin(punti_angolo));
+                double punti_centro_Y = Y_centro - (raggio * 0.95 * Math.Cos(punti_angolo));
+                orologio.FillEllipse(Brushes.Black, (float)punti_centro_X - 4, (float)punti_centro_Y - 4, 8, 8);
+            }
+            for (int i = 0; i < 60; i++)
+            {
+                double punti_angolo = i * Math.PI / 30;
+                double punti_centro_X = X_centro - raggio * 0.95 * Math.Sin(punti_angolo);
+                double punti_centro_Y = Y_centro - raggio * 0.95 * Math.Cos(punti_angolo);
+                orologio.FillRectangle(Brushes.Black, (float)punti_centro_X - 1, (float)punti_centro_Y - 1, 2, 2);
+            }
+
+            // Creazione dei numeri
+            for (int i = 1; i <= 12; i++)
+            {
+                double numeri_angolo = - i * Math.PI / 6;
+                double numeri_centro_X = X_centro - raggio * 0.82 * Math.Sin(numeri_angolo);
+                double numeri_centro_Y = Y_centro - raggio * 0.82 * Math.Cos(numeri_angolo);
+
+                Font font = new Font("Arial", 16);
+                Brush brush = new SolidBrush(Color.Black);
+                int numeri_X;
+                int numeri_Y;
+                if (i < 10)
+                {
+                    numeri_X = (int)numeri_centro_X - 10;
+                    numeri_Y = (int)numeri_centro_Y - 10;
+                } else
+                {
+                    numeri_X = (int)numeri_centro_X - 15;
+                    numeri_Y = (int)numeri_centro_Y - 15;
+                }
+                Point labelLocation = new Point(numeri_X, numeri_Y);
+                orologio.DrawString(i.ToString(), font, brush, labelLocation);
+            }
+
+            int h = DateTime.Now.Hour;
+            int min = DateTime.Now.Minute;
+            int sec = DateTime.Now.Second;
+
+            lancette(orologio, X_centro, Y_centro, raggio * 0.55, (h + min / 60.0) * 30, Pens.Black, (float)3.5); //Ore
+            lancette(orologio, X_centro, Y_centro, raggio * 0.85, min * 6, Pens.Black, (float)3.5); //Minuti
+            lancette(orologio, X_centro, Y_centro, raggio * 0.95, sec * 6, Pens.Orange, 2); //Secondi
+
+            Graphics c = this.CreateGraphics();
+            int X_cerchietto = ClientSize.Width / 2;
+            int Y_cerchietto = ClientSize.Height / 2;
+        }
+
+        private void lancette(Graphics orologio, int X_centro, int Y_centro, double length, double ampiezza, Pen pen, float size)
+        {
+            double angolo_rad = Math.PI / 180 * ampiezza; // angolo in radianti
+            int Coordinata_x = (int)(X_centro + length * Math.Sin(angolo_rad));
+            int Coordinata_y = (int)(Y_centro - length * Math.Cos(angolo_rad));
+            orologio.DrawLine(new Pen(pen.Color, size), X_centro, Y_centro, Coordinata_x, Coordinata_y);
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Interval = 1000;
+            lblData.Text = DateTime.Now.ToString();
+            this.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string imagePath = Path.Combine(Application.StartupPath, "..\\..\\..\\background.jpg");
+            this.BackgroundImage = Image.FromFile(imagePath);
+            this.BackgroundImageLayout = ImageLayout.Stretch;    
+        }
+        
+    }
+}
